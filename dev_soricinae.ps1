8ad8662c61f1d4480a5ed9ee9726f6411604352c
soricinae <soricinae@gmail.com>
2021-09-04 13:58:17 -0400

    Create dev_soricinae.ps1

---
+++ dev_soricinae.ps1
@@ -0,0 +1,36 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common dev settings for machine learning using only Windows native tools
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "SystemConfiguration.ps1";
+executeScript "FileExplorerSettings.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "Browsers.ps1";
+executeScript "Azure.ps1";
+executeScript "Node.ps1";
+executeScript "VisualStudio.ps1";
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
