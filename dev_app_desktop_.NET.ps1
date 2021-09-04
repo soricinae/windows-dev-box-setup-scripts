69a6c52fe23d4f6507614621c689bee8dea86c6e
soricinae <soricinae@gmail.com>
2021-09-04 13:10:07 -0400

    Update dev_app_desktop_.NET.ps1

--- dev_app_desktop_.NET.ps1
+++ dev_app_desktop_.NET.ps1
@@ -36,12 +36,12 @@ executeScript "CommonDevTools.ps1";
 # visualstudio2017professional
 # visualstudio2017enterprise
 
-choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
 Update-SessionEnvironment #refreshing env due to Git install
 
 #--- UWP Workload and installing Windows Template Studio ---
-choco install -y visualstudio2017-workload-azure
-choco install -y visualstudio2017-workload-manageddesktop
+choco install -y visualstudio2019-workload-azure
+choco install -y visualstudio2019-workload-manageddesktop
 
 #--- reenabling critial items ---
 Enable-UAC
