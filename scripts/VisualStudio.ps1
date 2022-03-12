01d47a60ccbe7a0c4fbdcadb1ff3bc616170b8d9
soricinae <soricinae@gmail.com>
2022-03-12 13:32:23 -0500

    Update VisualStudio.ps1

--- scripts/VisualStudio.ps1
+++ scripts/VisualStudio.ps1
@@ -1,6 +1,8 @@
-choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco install -y visualstudio2022community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
 Update-SessionEnvironment #refreshing env due to Git install
 
 #--- UWP Workload and installing Windows Template Studio ---
-choco install -y visualstudio2019-workload-azure
-choco install -y visualstudio2019-workload-manageddesktop
+choco install -y visualstudio2022-workload-azure
+choco install -y visualstudio2022-workload-manageddesktop
+choco install -y visualstudio2022-workload-netweb
+choco install -y visualstudio2022-workload-node
